101f82cbf2eed70ab74c5699dfc6c1f2b6e31e63
gemilank13 <39510051+gemilank13@users.noreply.github.com>
2021-04-01 12:46:44 +0700

    Delete opnsynid_stock_card_aeroo_report directory

--- opnsynid_stock_card_aeroo_report/reports/__init__.py
+++
@@ -1,5 +0,0 @@
-# -*- coding: utf-8 -*-
-# Copyright 2016 OpenSynergy Indonesia
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
-
-from . import stock_card, stock_move_in, stock_move_out, stock_move_history

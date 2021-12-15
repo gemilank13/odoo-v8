71149e2df441235cc2e27dd814b70bcfbd7ab1d0
gemilank13 <39510051+gemilank13@users.noreply.github.com>
2021-12-15 13:52:56 +0700

    Add files via upload

---
+++ ab_report_do/__openerp__.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+# Copyright 2018 Geger Gemilank
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+# pylint: disable=locally-disabled, manifest-required-author
+{
+    "name": "EDI Indonesia - Report Picking List",
+    "version": "8.0.1.0.0",
+    "category": "localization",
+    "website": "https://edi-indonesia.co.id",
+    "author": "Geger - PT EDI Indonesia",
+    "license": "AGPL-3",
+    "installable": True,
+    "depends": [
+        "stock","base","report"
+    ],
+    "data": ["views/report_do.xml","views/report.xml"],
+}
+
